d3ddbf024741ebaf953be1d56e973f76535c1dbe
mizphses <info@gentoo.app>
2020-10-23 20:21:07 +0900

    CI通りますように

--- pages/access.vue
+++ pages/access.vue
@@ -10,8 +10,8 @@
     <hr />
     <h3 class="title is-4 has-text-grey">アクセス方法</h3>
     <h4>
-      ⑴
-      JR岡山駅より岡電バスをご利用になられる場合 【最寄り：蓬莱橋・夢二郷土美術館前】
+      ⑴ JR岡山駅より岡電バスをご利用になられる場合
+      【最寄り：蓬莱橋・夢二郷土美術館前】
     </h4>
     <ul type="disc">
       <li>
@@ -21,18 +21,15 @@
       <li>
         <a
           href="http://www.okayama-kido.co.jp/bus/jikoku/pdf190316a/15_18_fujiwara.pdf"
-        >[18] 天満屋・後楽園経由 藤原団地 行（20分程度）
+        >
+          [18] 天満屋・後楽園経由 藤原団地 行（20分程度）
         </a>
       </li>
       <li>
-        <a href="http://www.okayama-kido.co.jp/korakuen/"
-          ><i
-            class="em em-peach"
-            aria-role="presentation"
-            aria-label="PEACH"
-          ></i
-          > 後楽園・夢二郷土美術館直行バス（10分程度）</a
-        >
+        <a href="http://www.okayama-kido.co.jp/korakuen/">
+          <i class="em em-peach" aria-role="presentation" aria-label="PEACH" />
+          後楽園・夢二郷土美術館直行バス（10分程度）
+        </a>
       </li>
     </ul>
     <h4>
@@ -53,7 +50,8 @@
     <h4>⑶ JR山陽本線【最寄り：西川原】</h4>
     <ul type="disc">
       <li>
-        山陽本線 「西川原・就実」駅で降車後「プラザホテル」に向かってください。プラザホテルに向かって右の道を通ります。
+        山陽本線
+        「西川原・就実」駅で降車後「プラザホテル」に向かってください。プラザホテルに向かって右の道を通ります。
       </li>
     </ul>
     <h4>タクシー・自家用車・徒歩</h4>
