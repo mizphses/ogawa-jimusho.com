90885b7593b882fe216e38cade0f72d7107cce67
mizphses <info@gentoo.app>
2020-10-25 10:24:36 +0900

    Remove Emoji

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,11 +25,6 @@ export default {
         href:
           'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap',
       },
-      {
-        rel: 'stylesheet',
-        tyle: 'text/css',
-        href: 'https://emoji-css.afeld.me/emoji.css',
-      },
     ],
   },
 
