beb7fe31c4cd86d83d0e829b49b08b31b9006d06
Fuminori Ogawa <info@gentoo.app>
2020-10-25 15:04:38 +0900

    nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,10 +4,9 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    __dangerouslyDisableSanitizers: ['script'],
     script: [
       {
-        innerHTML: `(function(d) {var config = {kitId: 'cte1jff',scriptTimeout: 3000,async: true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);`,
+        src: '//webfonts.sakura.ne.jp/js/sakurav3.js',
         type: 'text/javascript',
       },
     ],
