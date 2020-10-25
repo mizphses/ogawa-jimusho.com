144be508e2f68d51effa949cf249a26a562651ec
mizphses <info@gentoo.app>
2020-10-25 10:29:12 +0900

    meta-description

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '後楽園すぐ！岡山市の税理士事務所です．' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
